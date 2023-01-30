606d033732bc31041cfa86b73f6f6c7661df4f6b
Sharonchepko <123359783+Sharonchepko@users.noreply.github.com>
2023-01-30 15:05:27 +0300

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> 
+================================================================= RESTART: C:/Users/KOKO/arith.py =================================================================
+normal division 3.3333333333333335
+floor division 3
