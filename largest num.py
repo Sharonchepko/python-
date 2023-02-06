874e1c50cda3e24aa178aa97c2426226088ab10d
Sharonchepko <123359783+Sharonchepko@users.noreply.github.com>
2023-02-06 17:18:01 +0300

    Add files via upload

---
+++ largest num.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+num3 = int(input("enter third  number:"))
+if((num1>num2)and(num1>num3)):
+    print("num 1 is the largest")
+elif((num2>num1)and(num2>num3)):
+    print("num 2 is the largest")
+else:
+        print("num 3 is the largest")
+        
+          
